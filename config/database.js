731b727d72fe98b670c97c13dd5a05d5bcb00d0c
Bui Luan <builuan1404@gmail.com>
2025-10-07 18:09:43 +0700

    Update backend with latest fixes

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 module.exports.connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL);
-        console.log("Connection Success");
+        console.log("✅ MongoDB Connected!");
+        console.log("📡 Đang kết nối tới:", process.env.MONGO_URL);
     } catch (error) {
         console.log("Connection Error");
     }
